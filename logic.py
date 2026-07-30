edfba993c95a227b34a11c19f30719f9c4c5f1de
Laraib Faridi <faridilaraib250@gmail.com>
2026-07-30 13:32:05 +0500

    Initial commit

---
+++ logic.py
@@ -0,0 +1,11 @@
+
+
+numbers = [5, 2, 9, 1, 7]
+
+small = 0
+
+for i in numbers:
+    if numbers < i:
+     small = numbers[i]
+
+print(small)      
---
+++ main.py
@@ -0,0 +1,319 @@
+# print("Hello World")
+
+# marks = []
+
+# a = int(input("enter your marks"))
+# b = int(input("enter your marks"))
+# c = int(input("enter your marks"))
+# d = int(input("enter your marks"))
+# e = int(input("enter your marks"))
+
+# marks.insert(0, a)
+# marks.insert(1, b)
+# marks.insert(2, c)
+# marks.insert(3, d)
+# marks.insert(4, e)
+
+# marks.sort()
+# print("sorted marks:" , marks)
+
+# numbers = []
+
+# a = int(input("enter number 1"))
+# b = int(input("enter number 2"))
+# c = int(input("enter number 3"))
+# d = int(input("enter number 4"))
+# e = int(input("enter number 5"))
+
+# numbers.insert(0, a)
+# numbers.insert(1, b)
+# numbers.insert(2, c)
+# numbers.insert(3, d)
+# numbers.insert(4, e)
+
+# total = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]
+
+# print ("list:", numbers)
+# print ("total lsit:", total) 
+
+# a = (5,0,7,0,8,8,9,0,0,0,5,0,)
+
+# count_zero = a.count(0)
+
+# print("zero num in list are:" , count_zero)
+
+# numbers = []
+
+# a = input("Enter numbers : ")
+# b = input("Enter numbers : ")
+# c = input("Enter numbers : ")
+# d = input("Enter numbers : ")
+# e = input("Enter numbers : ")
+
+# set = {a,b,c,d,e}
+# numbers = set.union()
+# print (numbers)
+
+
+# userInput = input("Enter your words : ")
+# dic = {
+#     "kushamadeed" : "wellcome",
+#     "idhar ao" : "come here",
+#     "dobara jao": "go again",
+# }
+
+# print (dic.get(userInput))
+
+# c = {78,90,99,"73",False,779.3}
+# print(c)
+
+# s = set()
+# s.add(20)
+# s.add(20.0)
+# s.add('20')
+
+# print(s)
+# print(len(s))
+
+# s = {}
+# print(type(s))
+
+# friends = {}
+
+# name1 = input("Enter your name : ")
+# lan1 = input("Enter your langauge : ")
+
+# name2 = input("Enter your name : ")
+# lan2 = input("Enter your langauge : ")
+
+
+# name3 = input("Enter your name : ")
+# lan3 = input("Enter your langauge : ")
+
+# friends [name1] = lan1
+# friends [name2] = lan2
+# friends [name3] = lan3
+
+# print("Friends and thier favurite langauge")
+# print(friends)
+
+# friends = {}
+
+# friends ["ahmed"] = "java"
+# friends ["ahmed"] = "css"
+
+# print (friends)
+
+# friends ["ali"] = "css"
+# friends ["ahmed"] = "css"
+
+# print(friends)
+
+# s = {8, 7, 12, "Harry", [1,2]}
+# print(s)
+
+
+# user1 = input("Enter your number : ")
+# user2 = input("Enter your number : ")
+# user3 = input("Enter your number : ")
+# user4 = input("Enter your number : ")
+
+# if(user1 > user2 and user1 > user3 and user1 > user4):
+#     print("The bigg num is :", user1)
+
+# elif(user2 > user1 and user2 > user3 and user2 > user4):
+#     print("The bigg num is :", user2)
+
+# elif(user3 > user1 and user3 > user2 and user3 > user4):
+#     print("The bigg num is :", user3)
+
+# elif(user4 > user1 and user4 > user2 and user4 > user3):
+#     print("The bigg num is :", user4)
+
+# else:
+#     print("isn`t a bigg num in list") 
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+# d = int(input("Enter fourth number: "))
+
+# greatest = max(a, b, c, d)
+# print("Greatest number is:", greatest)
+
+
+# sub1 = int(input("Enter your marks of subejct 1 : "))
+# sub2 = int(input("Enter your marks of subejct 2 : "))
+# sub3 = int(input("Enter your marks of subejct 3 : "))
+ 
+
+# total_percentage = (sub1 + sub2 + sub3) 
+
+# if(sub1 >= 33 and sub2 >= 33 and sub3 >= 33 and total_percentage >= 40):
+#     print("You are clear!")
+
+# else:
+#     print("you are fail!")    
+
+
+# comment = input("Enter your comment : ").lower()
+
+# if("buy now" in comment or "click this" in comment or "subscribe this" in comment):
+#     print("this comment is spam")
+
+# else:
+#     print("this comment is not spam")    
+
+
+# userName = input("Enter your name : ")
+
+# if len(userName) <=10:
+#     print("username less then 10 character")
+
+# else:
+#     print("username less then 10 or more characters!")    
+
+
+# 90 – 100 => Ex
+# 80 – 90 => A
+# 70 – 80 => B
+# 60 – 70 =>C
+# 50 – 60 => D
+# <50 => F
+
+# marks = int(input("Enter your marks : "))
+
+# if(marks >= 90 and marks <= 100):
+#     grade = "Ex"
+
+# elif(marks >= 80):
+#     grade = "A"
+
+# elif(marks >= 70):
+#     grade = "B"
+
+# elif(marks >= 60):
+#     grade = "C"
+
+# elif(marks >= 50):
+#     grade = "D" 
+
+# else:
+#     grade = "F"
+
+# print("Your grade is :", grade)   
+
+
+# num1 = int(input("entr num 1 : "))
+# num2 = int(input("entr num 2 : "))
+
+# print("operations")
+# print("+ Addition")
+# print("- Subtraction")
+# print("* Multiplication")
+# print("/ Division")
+
+# op = input("opetation chose kro(+, -, *, /):")
+
+# if(op == '+'):
+#     print("Result:", num1 + num2)
+
+# elif(op == '-'):
+#     print("Result:", num1 - num2)
+
+# elif(op == '*'):
+#     print("Result:", num1 * num2)
+
+# elif(op == '/'):
+#     if num2 != 0:
+#         print("Result:", num1 // num2)
+#     else:
+#             print("eror 0 se devide hota")
+
+# else:
+#     print("invalid opertaions")                
+
+
+# user = int(input("Enter table number : "))
+# start = int(input("start kha se ho: "))
+# end = int(input("end kha pr ho : "))
+
+# i = start
+# while(i<=end):
+#     print(user, "X", i, "=", user*i)
+#     i = i+1
+
+
+# i=1
+# addition = 0
+# while(i<=30):
+#     print(addition+=1)
+#     i=i+1
+
+
+# print(addition)       
+
+
+# def fun(a):
+#     print("Wellcome", a)
+
+
+
+
+# def opr(a,b,c):
+#     if(c ==  '+'):
+#         print(a + b)
+
+#     elif(c == '-'):
+#         print(a - b)
+
+#     elif(c == '*'):
+#         print(a * b)
+
+#     elif(c == '/'):
+#         print(a / b)
+
+#     else:
+#         print("!?!") 
+
+
+# opr(3,5,"+")                       
+# opr(9,5,"-")                       
+# opr(7,5,"*")                       
+# opr(1,5,"/")                       
+
+
+# def table():
+#     i=1
+#     while(i<=10):
+
+
+# def allTables(num):
+#     table = ""
+
+#     for j in range(1,6)
+#         table += f"{num} X {j} = {num*j}"
+#         with open(f"tables/table_{num}","w")as f:
+#             f.write(table)
+
+    
+
+
+
+#      for i in range(1,11):
+#         allTables(i)
+
+
+
+
+# with open("file.txt","r") as f:
+#     change = f.read()
+
+# change = change.replace("Donkey","#####")
+
+
+# with open("file.txt","w") as f:
+#     f.write(change)
+
+
+# print("word replaced successfulll")    
